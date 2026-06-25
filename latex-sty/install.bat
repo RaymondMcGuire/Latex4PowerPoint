2b86f5449a97ef2ad979395d84d4d100ebe5c74c
raymondmg <raymondmg@foxmail.com>
2026-06-25 16:04:39 +0900

    upgrade to vs2026

--- latex-sty/install.bat
+++ latex-sty/install.bat
@@ -1,6 +1,6 @@
-mkdir %USERPROFILE%\texmf\tex\latex\local\cgmathnotation
+if not exist "%USERPROFILE%\texmf\tex\latex\local\cgmathnotation" mkdir "%USERPROFILE%\texmf\tex\latex\local\cgmathnotation"
 
-copy cgmathnotation.sty %USERPROFILE%\texmf\tex\latex\local\cgmathnotation\
+copy /Y "cgmathnotation_en.sty" "%USERPROFILE%\texmf\tex\latex\local\cgmathnotation\cgmathnotation.sty"
 
 initexmf --update-fndb
 
